6002deefed995dc40242bf976608449b2dbab086
Dzianis <dzianis.makeichyk@gmail.com>
2022-09-22 14:40:38 +0200

    chore: updt icons (#153)

--- src/common/index.ts
+++
@@ -1,4 +0,0 @@
-export { GlobalStyling } from './GlobalStyling';
-export { ThemedApp } from './ThemedApp';
-export type { ThemedAppProps, GlobalStylingOptions } from './ThemedApp';
-export type { GlobalStylingProps } from './GlobalStyling';
