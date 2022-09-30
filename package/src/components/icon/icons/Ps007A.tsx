cae01911a7d9d47090e7999aa6c80462082ad257
Dzianis <dzianis.makeichyk@gmail.com>
2022-09-30 13:52:58 +0200

    chore: icons with annotation in components (#183)

--- package/src/components/icon/icons/Ps007A.tsx
+++ package/src/components/icon/icons/Ps007A.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { SVGProps } from 'react';
 
 const SvgPs007A = (props: SVGProps<SVGSVGElement>) => (
-	<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#231f20" {...props}>
+	<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
 		<g id="PS007A">
 			<path id="Symbol" d="M7 2H17V3L8 3L8 21H17V22H7V2Z" />
 		</g>
