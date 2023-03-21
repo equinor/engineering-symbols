f0a289df6364edbdfb4a921740ff2e87a94d563c
Dzianis <dzianis.makeichyk@gmail.com>
2023-03-21 13:09:28 +0100

    fix: Radix on master (#347)

--- converter/index.js
+++ converter/index.js
@@ -1,6 +1,45 @@
-const { saveAs } = require('file-saver');
+const fs = require('fs');
 
-const blob = new Blob(['Hello, world!'], { type: 'text/plain;charset=utf-8' });
-console.log(blob);
+const lib = require('../__FIXTURE__/symbol-library.json');
 
-saveAs(blob, 'hello world.txt');
+// Make TSX?
+
+async function convertingApiToSvg() {
+	try {
+		// const res = await fetch('https://api.publicapis.org/entries');
+		// const res = await fetch(lib);
+		// const json = await res.json();
+
+		const { id, geometryString, width, height, connectors } = lib.ND0001;
+		const hasConnectors = connectors && connectors.length > 0;
+
+		const content = `
+      <svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.org/2000/svg">
+        <g id="Symbol">
+          <path d="${geometryString}" />
+        </g>
+        ${
+			hasConnectors &&
+			`<g id="Annotations">
+            ${connectors
+				.map(({ relativePosition, id }) => {
+					return `<circle
+                  key="annotation-connector-${id}"
+                  id="annotation-connector-${id}"
+                  cx="${relativePosition.x}"
+                  cy="${relativePosition.y}"
+                  r="0.5"
+                />`;
+				})
+				.join('')}
+          </g>`
+		}
+      </svg>
+    `;
+		await fs.writeFileSync(`converter/test/${id}.svg`, content);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+convertingApiToSvg();
