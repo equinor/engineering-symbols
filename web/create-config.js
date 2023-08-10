03455dfcc7097a2429c36f02aa16b117d817665c
dzianismakeichyk <dzianis.makeichyk@gmail.com>
2023-08-10 09:00:04 +0200

    chore: config.json for Radix

---
+++ web/create-config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const fs = require('fs');
+
+const config = {
+	MSAL_CLIENT_ID: process.env.NEXT_PUBLIC_MSAL_CLIENT_ID,
+	MSAL_AUTHORITY: process.env.NEXT_PUBLIC_MSAL_CLIENT_ID,
+};
+
+fs.writeFileSync('config.json', JSON.stringify(config));
+console.log('⚡️', 'config.json created successfully');
