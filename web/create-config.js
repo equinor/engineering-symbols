c9d49a36653ad001273bdd585d92cb613af27a6b
dzianismakeichyk <dzianis.makeichyk@gmail.com>
2023-08-11 08:45:03 +0200

    chore: radix pima

--- web/create-config.js
+++ web/create-config.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const config = {
 	MSAL_CLIENT_ID: process.env.NEXT_PUBLIC_MSAL_CLIENT_ID || '',
-	MSAL_AUTHORITY: process.env.NEXT_PUBLIC_MSAL_CLIENT_ID || '',
+	MSAL_AUTHORITY: process.env.NEXT_PUBLIC_MSAL_AUTHORITY || '',
 };
 
 fs.writeFileSync('config.json', JSON.stringify(config));
