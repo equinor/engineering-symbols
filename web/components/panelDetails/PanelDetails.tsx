33fbc21964f3b1a12a3d6ad7dad17197c3f78734
dzianismakeichyk <dzianis.makeichyk@gmail.com>
2023-09-15 08:40:22 +0200

    remove memo

--- web/components/panelDetails/PanelDetails.tsx
+++ web/components/panelDetails/PanelDetails.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, memo, useRef } from 'react';
+import { FunctionComponent, useRef } from 'react';
 import { Formik, FormikProps } from 'formik';
 import { useMsal } from '@azure/msal-react';
 
@@ -18,77 +18,81 @@ type PanelDetailsComponentProps = {
 	symbol: SymbolsProps;
 };
 
-export const PanelDetailsComponent = memo(
-	({ setUpdateDraftSymbol, updateCurrentSymbol, enableReinitialize, onClosePanel, symbol }: PanelDetailsComponentProps) => {
-		const { key, description, width, height, geometry, connectors } = symbol;
+export const PanelDetailsComponent: FunctionComponent<PanelDetailsComponentProps> = ({
+	setUpdateDraftSymbol,
+	updateCurrentSymbol,
+	enableReinitialize,
+	onClosePanel,
+	symbol,
+}): JSX.Element => {
+	const { key, description, width, height, geometry, connectors } = symbol;
 
-		const formRef = useRef<FormikProps<any>>(null);
+	const formRef = useRef<FormikProps<any>>(null);
 
-		const { instance } = useMsal();
+	const { instance } = useMsal();
 
-		const { tenantId } = instance.getActiveAccount() as any;
+	const { tenantId } = instance.getActiveAccount() as any;
 
-		const onSubmitForm = () => {
+	const onSubmitForm = () => {
+		if (!formRef.current) return;
+
+		formRef.current.handleSubmit();
+	};
+
+	const onFormChange = () => {
+		const timer = setTimeout(() => {
 			if (!formRef.current) return;
 
-			formRef.current.handleSubmit();
+			updateCurrentSymbol(formRef.current.values);
+		}, 1);
+
+		return () => {
+			clearTimeout(timer);
 		};
+	};
 
-		const onFormChange = () => {
-			const timer = setTimeout(() => {
-				if (!formRef.current) return;
+	// const isUniqueID = (obj: any, name: string, id: string) => obj.filter((sbl: { [x: string]: string }) => sbl[name] === id).length <= 0;
 
-				updateCurrentSymbol(formRef.current.values);
-			}, 1);
+	return (
+		<PanelDetailsStyled isShow>
+			<PanelDetailsWrapperStyled>
+				<EditPanelStyled />
 
-			return () => {
-				clearTimeout(timer);
-			};
-		};
-
-		// const isUniqueID = (obj: any, name: string, id: string) => obj.filter((sbl: { [x: string]: string }) => sbl[name] === id).length <= 0;
-
-		return (
-			<PanelDetailsStyled isShow>
-				<PanelDetailsWrapperStyled>
-					<EditPanelStyled />
-
-					<EditFromStyled>
-						<PanelDetailsButtons>
-							<ButtonComponent size="s" type="submit" onClick={() => onSubmitForm()}>
-								Save
-							</ButtonComponent>
-							<ButtonComponent size="s" onClick={() => onClosePanel()} appearance="secondary">
-								Cancel
-							</ButtonComponent>
-						</PanelDetailsButtons>
-						<Formik
-							// enableReinitialize={enableReinitialize}
-							// For symbol swithching
-							enableReinitialize={true}
-							innerRef={formRef}
-							initialValues={{
-								...symbol,
-								key,
-								description,
-								width,
-								height,
-								owner: tenantId,
-								geometry,
-								connectors,
-							}}
-							onSubmit={(values, { setSubmitting }) => {
-								setTimeout(() => {
-									setSubmitting(false);
-									setUpdateDraftSymbol(values);
-									console.log('⚡️', 'onSubmin value:', values);
-								}, 400);
-							}}>
-							<EditFormComponent updateSymbol={updateCurrentSymbol} formChange={onFormChange} />
-						</Formik>
-					</EditFromStyled>
-				</PanelDetailsWrapperStyled>
-			</PanelDetailsStyled>
-		);
-	}
-);
+				<EditFromStyled>
+					<PanelDetailsButtons>
+						<ButtonComponent size="s" type="submit" onClick={() => onSubmitForm()}>
+							Save
+						</ButtonComponent>
+						<ButtonComponent size="s" onClick={() => onClosePanel()} appearance="secondary">
+							Cancel
+						</ButtonComponent>
+					</PanelDetailsButtons>
+					<Formik
+						// enableReinitialize={enableReinitialize}
+						// For symbol swithching
+						enableReinitialize={true}
+						innerRef={formRef}
+						initialValues={{
+							...symbol,
+							key,
+							description,
+							width,
+							height,
+							owner: tenantId,
+							geometry,
+							connectors,
+						}}
+						onSubmit={(values, { setSubmitting }) => {
+							setTimeout(() => {
+								setSubmitting(false);
+								setUpdateDraftSymbol(values);
+								console.log('⚡️', 'onSubmin value:', values);
+							}, 400);
+						}}>
+						<EditFormComponent updateSymbol={updateCurrentSymbol} formChange={onFormChange} />
+					</Formik>
+				</EditFromStyled>
+			</PanelDetailsWrapperStyled>
+		</PanelDetailsStyled>
+	);
+};
