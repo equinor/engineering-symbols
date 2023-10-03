5dc7e6006b0da4ca24d83f8fa6daeb0d1877d837
Lorentz F. Barstad <29151133+lorentzfb@users.noreply.github.com>
2023-10-03 14:17:54 +0200

    fix: use nanoid instead of math.random

--- web/components/symbolEditor/utils/strings.ts
+++ web/components/symbolEditor/utils/strings.ts
@@ -1,9 +1,6 @@
+import { customAlphabet } from 'nanoid';
+
 export function generateRandomString(length: number): string {
-	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-	let result = '';
-	for (let i = 0; i < length; i++) {
-		const randomIndex = Math.floor(Math.random() * characters.length);
-		result += characters[randomIndex];
-	}
-	return result;
+	const nanoid = customAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', 10);
+	return nanoid(length);
 }
