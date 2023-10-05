1b1bc36bf50060cabc6088a09211a944d2b10ab6
dzianismakeichyk <dzianis.makeichyk@gmail.com>
2023-10-05 10:35:42 +0200

    chore: ID | Name - unique

--- web/components/symbolEditor/utils/strings.ts
+++ web/components/symbolEditor/utils/strings.ts
@@ -1,6 +1,15 @@
 import { customAlphabet } from 'nanoid';
 
-export function generateRandomString(length: number): string {
+import { SymbolConnectorInternal } from '../models/SymbolData';
+
+export function generateRandomString(length: number, connectors: SymbolConnectorInternal[]): string {
 	const nanoid = customAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', 10);
-	return nanoid(length);
+	let newId = nanoid(length);
+
+	// Check if the generated ID already exists, generate a new one until unique
+	while (connectors.some((item) => item.id === newId)) {
+		newId = nanoid(length);
+	}
+
+	return newId;
 }
