2fdfcfc654284c2e334be307dbc9314fe2619716
Dzianis <dzianis.makeichyk@gmail.com>
2023-12-19 13:13:25 +0100

    fix: no env (#848)

--- web/api/API.ts
+++ web/api/API.ts
@@ -1,18 +1,31 @@
 import { fetchApi, fetchFileApi } from './fetchAPI';
 
+let environment = 'dev';
+
+if (typeof window !== 'undefined') {
+	const prodUrl = 'engineering-symbols.equinor.com';
+	environment = !window.location.origin.includes(prodUrl) ? 'prod' : 'dev';
+}
+
 export const uploadSvgFile = (svgFile: FormData, validationOnly: boolean, contentType: string) =>
-	fetchFileApi({ path: `https://dev-engsym-api.azurewebsites.net/manage/symbols?validationOnly=${validationOnly}`, data: svgFile, contentType });
+	fetchFileApi({
+		path: `https://${environment}-engsym-api.azurewebsites.net/manage/symbols?validationOnly=${validationOnly}`,
+		data: svgFile,
+		contentType,
+	});
 
-export const getSymbolsQuery = () => fetchApi({ path: 'https://dev-engsym-api.azurewebsites.net/symbols?onlyLatestVersion=false' });
+export const getSymbolsQuery = () => fetchApi({ path: `https://${environment}-engsym-api.azurewebsites.net/symbols?onlyLatestVersion=false` });
 
-export const getManageSymbolsQuery = () => fetchApi({ path: 'https://dev-engsym-api.azurewebsites.net/manage/symbols?onlyLatestVersion=false' });
+export const getManageSymbolsQuery = () =>
+	fetchApi({ path: `https://${environment}-engsym-api.azurewebsites.net/manage/symbols?onlyLatestVersion=false` });
 
-export const deleteSymbol = (id: string) => fetchApi({ path: `https://dev-engsym-api.azurewebsites.net/manage/symbols/${id}`, method: 'delete' });
+export const deleteSymbol = (id: string) =>
+	fetchApi({ path: `https://${environment}-engsym-api.azurewebsites.net/manage/symbols/${id}`, method: 'delete' });
 
 // New svg only (?)
 export const updateSymbol = (id: string, data: string) =>
 	fetchFileApi({
-		path: `https://dev-engsym-api.azurewebsites.net/manage/symbols${id ? `/${id}` : ''}`,
+		path: `https://${environment}-engsym-api.azurewebsites.net/manage/symbols${id ? `/${id}` : ''}`,
 		data,
 		contentType: 'application/json',
 		method: id ? 'put' : 'post',
@@ -20,7 +33,7 @@ export const updateSymbol = (id: string, data: string) =>
 
 export const updateStatusSymbol = (id: string, data: string) =>
 	fetchFileApi({
-		path: `https://dev-engsym-api.azurewebsites.net/manage/symbols/${id}/status`,
+		path: `https://${environment}-engsym-api.azurewebsites.net/manage/symbols/${id}/status`,
 		data,
 		contentType: 'application/json',
 		method: 'put',
--- web/utils/authConfig.ts
+++ web/utils/authConfig.ts
@@ -1,13 +1,10 @@
 // Config object to be passed to Msal on creation
 import { Configuration } from '@azure/msal-browser';
-import getConfig from 'next/config';
-
-const { publicRuntimeConfig } = getConfig();
 
 export const msalConfig: Configuration = {
 	auth: {
-		clientId: publicRuntimeConfig.NEXT_PUBLIC_MSAL_CLIENT_ID,
-		authority: publicRuntimeConfig.NEXT_PUBLIC_MSAL_AUTHORITY,
+		clientId: '2e4ccc3b-9d87-4a03-af5f-ae1188027d40',
+		authority: 'https://login.microsoftonline.com/3aa4a235-b6e2-48d5-9195-7fcf05b459b0',
 		redirectUri: '/',
 		postLogoutRedirectUri: '/',
 	},
@@ -18,7 +15,7 @@ export const msalConfig: Configuration = {
 
 // Add here scopes for id token to be used at MS Identity Platform endpoints.
 export const loginRequest = {
-	scopes: [publicRuntimeConfig.NEXT_PUBLIC_API_SCOPE],
+	scopes: ['api://7584f051-6987-4c51-861d-77710537bd06/user_impersonation'],
 };
 
 // Add here the endpoints for MS Graph API services you would like to use.
